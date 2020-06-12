0051fc99e274e0e7f169702ad833a5ab51ceff15
Rajya Maram <rajyamaram@gmail.com>
2020-06-12 19:40:12 +0200

    gnore mvn folder

---
+++ src/main/java/com/rabobank/customerstatements/CustomerstatementsApplication.java
@@ -0,0 +1,13 @@
+package com.rabobank.customerstatements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerstatementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerstatementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabobank/customerstatements/CustomerstatementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabobank.customerstatements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerstatementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
